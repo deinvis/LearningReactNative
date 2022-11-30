c44af9bf451780ea2cccc82cd1e5051419dbff92
deinvis <davis-oliveira@hotmail.com.br>
2022-11-29 23:17:32 -0300

    New pages added

--- src/Pages/Start/index.jsx
+++ src/Pages/Start/index.jsx
@@ -1,25 +1,29 @@
 import React from "react";
-import { View, Text, Image, ScrollView, StyleSheet } from "react-native";
-import DefaultButton from "../../Components/Commom/DefaultButton";
-import LifeStatus from "../../Components/Commom/LifeStatus";
+import { View, Text, StyleSheet, Image, ScrollView } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Start(){
+import DefaultButton from "../../Components/Common/DefaultButton";
+import LifeStatus from "../../Components/Common/LifeStatus";
+
+export default function Start() {
+  const navigation = useNavigation();
+
   const handleNavAppExplanation = () => {
-    console.log("Testando o click");
+    console.log("Testando o clique");
   };
 
   return (
-    <View style={StyleSheet.container}>
+    <View style={styles.container}>
       <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
-        <View style={{alignItems: "center"}}>
+        <View style={{ alignItems: "center" }}>
           <Image
             source={require("../../assets/icons/logo3.png")}
             style={styles.logo}
           />
           <LifeStatus />
           <Text style={styles.description}>
-            Hello World! {"\n"}Primeiro app com React Native!
+            Vamos transformar sua vida {"\n"} em um jogo, buscando sempre {"\n"}{" "}
+            o melhor nível.
           </Text>
           <DefaultButton
             buttonText={"Continuar"}
@@ -34,22 +38,20 @@ export default function Start(){
 }
 
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: "rgba(21, 21, 21, 0.98)"
-    },
-
-    logo: {
-      width: 300,
-      height: 60,
-      marginTop: 60,
-      marginBottom: 20
-    },
-
-    description: {
-      color: "#FFFFFF",
-      fontSize: 20,
-      textAlign: "center",
-      marginVertical: 60
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: "rgba(21, 21, 21, 0.98)",
+  },
+  logo: {
+    width: 300,
+    height: 60,
+    marginTop: 60,
+    marginBottom: 20,
+  },
+  description: {
+    color: "#FFFFFF",
+    fontSize: 20,
+    textAlign: "center",
+    marginVertical: 60,
+  },
+});
--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
+
 import AllPages from "./AllPages";
 
-export default function Routes(){
-    return <AllPages/>;
+export default function Routes() {
+  return <AllPages />;
 }
