77c8a9ec0ceb01c3f3fd0e9f2ea9a4e66334f8aa
deinvis <davis-oliveira@hotmail.com.br>
2022-12-03 13:00:44 -0300

    Routes and home page structure

--- src/Routes/index.jsx
+++ src/Routes/index.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-
+import HomePage from "./HomePage";
 import AllPages from "./AllPages";
 
 export default function Routes() {
-  return <AllPages />;
+  const [showHome, setShowHome] = useState["false"];
+  return <> {showHome === "true" ? <HomePage /> : <AllPages /> } </>;
 }
